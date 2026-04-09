280865493d1c0cb821da96ac48e1fd5e6a77f601
Gael <angel.ga@northeastern.edu>
2026-04-09 13:13:31 -0400

    basic get user route

--- api/backend/routes/user_routes.py
+++ api/backend/routes/user_routes.py
@@ -1,10 +1,36 @@
-from flask import Blueprint, current_app
+from flask import Blueprint, current_app, jsonify
+from mysql.connector import Error
+from backend.db_connection import get_db
 
 
 user_routes = Blueprint("users", __name__)
 
 
 @user_routes.route("/")
-def get_root():
+def get_root() -> str:
     current_app.logger.info("GET /users/ handler")
     return "<h1>Users route</h1>"
+
+
+@user_routes.route("/<user_id>", methods=["GET"])
+def get_user_profile(user_id: str):
+    cursor = get_db().cursor(dictionary=True)
+    try:
+        current_app.logger.info(f"GET /users/{user_id}")
+
+        query = "SELECT * FROM users WHERE user_id = %s" % user_id
+        cursor.execute(query)
+        user = cursor.fetchone()
+
+        # ensure user exists
+        if not user:
+            return jsonify({"error": "Not found"}), 404
+
+        current_app.logger.info(f"Retrieved user {user_id}")
+        return jsonify(user), 200
+
+    except Error as e:
+        current_app.logger.error(f"Database error in get_user_profile(): {e}")
+        return jsonify({"error": "Unexpected error"}), 500
+    finally:
+        cursor.close()
