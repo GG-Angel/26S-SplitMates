fe644e94e4a653ab1e2871c120bd93e8fe620d84
Gael <angel.ga@northeastern.edu>
2026-04-10 17:26:27 -0400

    create mock groups

--- seeder/README.md
+++ seeder/README.md
@@ -5,3 +5,9 @@ Run the following command to seed the database with mock data:
 ```bash
 docker-compose --profile seed build seeder && docker-compose --profile seed run --rm seeder
 ```
+
+All in one command to reset the database with mock data:
+
+```bash
+docker compose down db -v && docker compose up db -d && docker-compose --profile seed build seeder && docker-compose --profile seed run --rm seeder
+```
--- seeder/seed.py
+++ seeder/seed.py
@@ -1,10 +1,14 @@
 import os
+import random
 import time
 from faker import Faker
 import mysql.connector
 
 fake = Faker()
-Faker.seed(42)
+
+# seed for consistent mock data
+Faker.seed(1)
+random.seed(1)
 
 
 def generate_mock_users(count: int = 50):
@@ -21,6 +25,20 @@ def generate_mock_users(count: int = 50):
     ]
 
 
+def generate_mock_groups(user_ids: list, count: int = 10):
+    return [
+        (
+            random.choice(user_ids),  # group_leader
+            fake.sentence(nb_words=3).rstrip("."),  # name
+            fake.street_address(),  # address
+            fake.city(),  # city
+            fake.state_abbr(),  # state
+            int(fake.zipcode()),  # zip_code
+        )
+        for _ in range(count)
+    ]
+
+
 def seed_db():
     print("Connecting to the database...")
     conn = mysql.connector.connect(
@@ -32,15 +50,33 @@ def seed_db():
     )
     cursor = conn.cursor()
 
-    users = generate_mock_users()
-    query = """
+    # --- Users ---
+    users = generate_mock_users(50)
+    cursor.executemany(
+        """
         INSERT INTO users (first_name, last_name, email, is_admin, is_analyst, created_at)
         VALUES (%s, %s, %s, %s, %s, %s)
-    """
-
-    print(f"Inserting {len(users)} mock users...")
-    cursor.executemany(query, users)
+    """,
+        users,
+    )
     conn.commit()
+    cursor.execute("SELECT user_id FROM users")
+    user_ids = [tuple(row)[0] for row in cursor.fetchall()]
+    print(f"  ✔ Seeded {len(user_ids)} users")
+
+    # --- Groups ---
+    groups = generate_mock_groups(user_ids, count=10)
+    cursor.executemany(
+        """
+        INSERT INTO `groups` (group_leader, name, address, city, state, zip_code)
+        VALUES (%s, %s, %s, %s, %s, %s)
+    """,
+        groups,
+    )
+    conn.commit()
+    cursor.execute("SELECT group_id FROM `groups`")
+    group_ids = [tuple(row)[0] for row in cursor.fetchall()]
+    print(f"  ✔ Seeded {len(group_ids)} groups")
 
     cursor.close()
     conn.close()
@@ -48,4 +84,7 @@ def seed_db():
 
 
 if __name__ == "__main__":
+    sleep_time = 7
+    print(f"Sleeping for {sleep_time} seconds...")
+    time.sleep(sleep_time)
     seed_db()
