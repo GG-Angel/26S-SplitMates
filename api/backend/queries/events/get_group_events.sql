acf3cbb6b867b32bc5051b775318681158f9cd45
MatthewEllis7 <ellis.matth@northeastern.edu>
2026-04-13 01:10:04 -0400

    fixed events page logic to show all events starting today or later instead of current time or later

--- api/backend/queries/events/get_group_events.sql
+++ api/backend/queries/events/get_group_events.sql
@@ -2,5 +2,5 @@ SELECT e.*, u.first_name, u.last_name
 FROM events e
 JOIN users u ON e.created_by = u.user_id
 WHERE e.group_id = %(group_id)s
-  AND e.starts_at >= NOW()
+  AND e.starts_at >= CURDATE()
 ORDER BY e.starts_at ASC;
