7e0adbc85e53db575c79c32aa8536f441492dac3
shawntribuce <shawntribuce@gmail.com>
2026-04-18 01:49:59 -0400

    feat: add invitation seed data

---
+++ database-files/splitmates_zz_4_invitations.sql
@@ -0,0 +1,4 @@
+USE `splitmates_db`;
+INSERT INTO invitations (group_id, sent_to, was_accepted) VALUES (1, 3, FALSE);
+INSERT INTO invitations (group_id, sent_to, was_accepted) VALUES (1, 4, FALSE);
+INSERT INTO invitations (group_id, sent_to, was_accepted) VALUES (2, 5, FALSE);
